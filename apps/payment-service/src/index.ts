fdf3da7dbc6bf1cc73dbd547c852c86e55f26ef4
Sergi0let <s.vashkevych@gmail.com>
2026-03-07 15:54:56 +0200

    add payment service

---
+++ apps/payment-service/src/index.ts
@@ -0,0 +1,27 @@
+import { serve } from '@hono/node-server';
+import { Hono } from 'hono';
+
+const app = new Hono();
+
+app.get('/', (c) => {
+  return c.text('Hello Hono!');
+});
+
+const start = async () => {
+  try {
+    serve(
+      {
+        fetch: app.fetch,
+        port: 3000,
+      },
+      (info) => {
+        console.log(`Payment Service is running on port 8002`);
+      }
+    );
+  } catch (error) {
+    console.log('Error:', error);
+    process.exit(1);
+  }
+};
+
+start();
