dca943fefd70a88d6f8b02264017ed79a55a4009
Sergi0let <s.vashkevych@gmail.com>
2026-03-08 15:17:41 +0200

    add auth product service expressjs

--- apps/product-service/src/index.ts
+++ apps/product-service/src/index.ts
@@ -1,13 +1,17 @@
+import { clerkMiddleware, getAuth } from '@clerk/express';
 import cors from 'cors';
 import express, { Request, Response } from 'express';
 
 const app = express();
+
 app.use(
   cors({
-    origin: ['http://localhost:3002', 'http://localhost:3003'],
+    origin: ['http://localhost:3000', 'http://localhost:3003'],
     credentials: true,
   })
 );
+
+app.use(clerkMiddleware());
 
 app.get('/health', (req: Request, res: Response) => {
   res.status(200).json({
@@ -15,6 +19,17 @@ app.get('/health', (req: Request, res: Response) => {
     uptime: process.uptime(),
     timestamp: Date.now(),
   });
+});
+
+app.get('/test', (req, res) => {
+  const auth = getAuth(req);
+  console.log(auth);
+  const userId = auth.userId;
+
+  if (!userId) {
+    return res.status(401).json({ message: 'You are not logged in!' });
+  }
+  res.json({ message: 'Product service authenticated' });
 });
 
 app.listen(8000, () => {
